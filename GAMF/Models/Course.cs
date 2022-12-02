c187089b4c5be732252b0cc34020161f94c08be7
haragaakos <83420576+haragaakos@users.noreply.github.com>
2022-12-02 22:49:28 +0100

    Add project files.

---
+++ GAMF/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace GAMF.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+}
---
+++ GAMF/Models/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿namespace GAMF.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+}
